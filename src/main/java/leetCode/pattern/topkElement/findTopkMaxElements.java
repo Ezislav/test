a50c3cd4b28a849fc1c963034d2a3de691af3a28
Ezislav <edvarganov@gmail.com>
2024-07-04 12:52:14 +0300

    add LeetCode Pattern upd

---
+++ src/main/java/leetCode/pattern/topkElement/findTopkMaxElements.java
@@ -0,0 +1,57 @@
+package leetCode.pattern.topkElement;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class findTopkMaxElements {
+
+//      Пирамидальная сортировка (Heap)
+//      Идея: Использовать приоритетную очередь (min-heap или max-heap) для хранения K наибольших/наименьших элементов.
+//
+//      Пример задачи:
+//      Найти K наибольших элементов в массиве.
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+
+        int[] result = findTopkMaxElements(nums, k);
+        System.out.println(Arrays.toString(result));  // Output: [5, 6]
+    }
+
+    private static int[] findTopkMaxElements(int[] nums, int k) {
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(k, Comparator.reverseOrder()); //Мы создаем приоритетную очередь, которая будет использоваться как минимальная куча (min-heap).
+
+        //Так как мы будем в этой задаче искать максимальные элементы в массиве, нам нужно указать компаратор (обратный порядок).
+        //По дефолту PriorityQueue - очередь, в которой лежат значения сначала минимальные и по возрастанию.
+
+
+        for (int num : nums) {
+
+            if (minHeap.size() < k) {
+                minHeap.add(num);     // Заполняем кучу первыми k элементами массива.
+            }
+
+//      Обновление кучи после достижения размера k
+//      Если размер кучи уже достиг k, мы сравниваем текущий элемент num с наименьшим элементом в куче (minHeap.peek()).
+
+//      Если num больше наименьшего элемента в куче, то:
+
+            else if (num < minHeap.peek()) {
+                minHeap.poll();  // Мы удаляем наименьший элемент из кучи с помощью minHeap.poll().
+                minHeap.add(num); // Добавляем текущий элемент num в кучу с помощью minHeap.add(num).
+
+            }
+        }
+
+//      Заполняем массив result значениями из очереди
+
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = minHeap.poll();
+        }
+        return result;
+    }
+}
--- src/main/java/leetCode/pattern/topkElement/findTopkMinElements.java
+++ src/main/java/leetCode/pattern/topkElement/findTopkMinElements.java
@@ -3,7 +3,7 @@ package leetCode.pattern.topkElement;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-public class PiramidalSorting {
+public class findTopkMinElements {
 
 //      Пирамидальная сортировка (Heap)
 //      Идея: Использовать приоритетную очередь (min-heap или max-heap) для хранения K наибольших/наименьших элементов.
@@ -15,11 +15,11 @@ public class PiramidalSorting {
         int[] nums = {3, 2, 1, 5, 6, 4};
         int k = 2;
 
-        int[] result = findTopKElements(nums, k);
+        int[] result = findTopkMaxElements(nums, k);
         System.out.println(Arrays.toString(result));  // Output: [5, 6]
     }
 
-    private static int[] findTopKElements(int[] nums, int k) {
+    private static int[] findTopkMaxElements(int[] nums, int k) {
 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(k); //Мы создаем приоритетную очередь, которая будет использоваться как минимальная куча (min-heap).
 
